43124458e90dcf32739684c3218ee2c2bcdc90a6
Dylan Jackson <dylan.jackson@student.swic.edu>
2026-02-19 12:51:00 -0600

    Add personal information variables

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun (plain JS)!");
+// Personal Information
+const dylan = "YourName";
+const jackson = "YourLastName";
+const age = 25; // Your actual age
+const isStudent = true;
+
+// Using template literals
+const introduction = `Hello! My name is $Dylan $Jackson. I am ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+// Print to console
+console.log(introduction);
+console.log(studentStatus);
